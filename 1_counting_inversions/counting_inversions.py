e89f4f15c4e5bd6c3ef8069a8e94861f98a67071
bellum <bellumm@gmail.com>
2014-05-10 02:33:25 +0300

    util.timeit was added. presaving length of arrays in counting inversions.

--- 1_counting_inversions/counting_inversions.py
+++ 1_counting_inversions/counting_inversions.py
@@ -1,16 +1,13 @@
 __author__ = 'bell'
 
-import os
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-nums = [int(line.strip()) for line in open(os.path.join(__location__, 'IntegerArray.txt'))]
-
 
 def sort_and_count(a, n):
     if n == 1:
         return a, 0
     else:
-        (b, x) = sort_and_count(a[:n/2], n/2)
-        (c, y) = sort_and_count(a[n/2:], n - n/2)
+        half = n/2
+        (b, x) = sort_and_count(a[:half], half)
+        (c, y) = sort_and_count(a[half:], n - half)
         (d, z) = merge_and_count_split_inv(b, c, n)
         return d, x + y + z
 
@@ -19,18 +16,17 @@ def merge_and_count_split_inv(b, c, n):
     d = []
     i = j = 0
     inv = 0
+    len_b = len(b)  # without pre-saved len values
+    len_c = len(c)  # run time was .57 sec -> .43 sec
     for k in range(n):
-        if j >= len(c) or (i < len(b) and b[i] < c[j]):
+        if j >= len_c or (i < len_b and b[i] < c[j]):
             d.append(b[i])
             i += 1
         else:
             d.append(c[j])
             j += 1
-            inv += len(b) - i
+            inv += len_b - i
     return d, inv
-
-(sorted_array, inversions) = sort_and_count(nums, len(nums))
-print inversions  # 2407905288
 
 
 def brute_force(array):
@@ -40,3 +36,16 @@ def brute_force(array):
             if array[i] > array[j]:
                 inv += 1
     return inv
+
+if __name__ == '__main__':
+    import os
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    nums = [int(line.strip()) for line in open(os.path.join(__location__, 'IntegerArray.txt'))]
+
+    from util.timeit import timeit
+
+    @timeit
+    def run():
+        (sorted_array, inversions) = sort_and_count(nums, len(nums))
+        print inversions  # 2407905288
+    run()
